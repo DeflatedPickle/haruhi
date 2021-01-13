1d5ab5c1842f4b82e6d7012f640f8aec8528c444
DeflatedPickle <deflatedpickle@gmail.com>
2021-01-13 06:24:44 +0000

    Changed RegistryUtil's value type from Any to *

--- src/main/kotlin/com/deflatedpickle/haruhi/util/RegistryUtil.kt
+++ src/main/kotlin/com/deflatedpickle/haruhi/util/RegistryUtil.kt
@@ -4,4 +4,4 @@ package com.deflatedpickle.haruhi.util
 
 import com.deflatedpickle.haruhi.api.Registry
 
-object RegistryUtil : Registry<String, Registry<String, Any>>()
+object RegistryUtil : Registry<String, Registry<String, *>>()
